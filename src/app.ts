330aad50124325c8f14a9fe92fa20da2cb2148b7
Julien Ferand <hello@julienferand.dev>
2022-03-17 21:52:44 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+
+// Routers
+import indexRouter from "@/routes/Index";
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  // handle it how it pleases you
+  res.status(404).json({ message: "not_found" });
+});
+
+// error handler
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
