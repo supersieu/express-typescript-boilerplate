4073cea459aa9350d5fc533ff39712117656f217
peutt <aymericqueret@gmail.com>
2022-05-05 14:52:11 +0200

    actuatorControllerRefactoring

--- src/controllers/actuatorController.ts
+++ src/controllers/actuatorController.ts
@@ -1,3 +1,4 @@
+import { Database } from "@/modules/Database";
 import { NextFunction, Request, Response } from "express";
 import models from "../models";
 import { ApiResponse } from "../modules/Response";
@@ -5,30 +6,22 @@ import {
   ActuatorPostSchema,
   ActuatorUpdateSchema,
 } from "../types/actuatorSchema";
+let database=new Database;
 var xss = require("xss");
 export default {
   get: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let actuators = [];
-      if (req.query.type) {
-        actuators = await models.Actuator.find({ type: req.query.type });
-      } else {
-        actuators = await models.Actuator.find({});
-      }
-      res
-        .status(200)
-        .json(new ApiResponse(res.statusCode.toString(), actuators));
+      const data=await database.get("actuator",req);
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      return;
     } catch (error) {
       next(error);
     }
   },
   get_by_id: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const actuator = await models.Actuator.findById({ _id: req.params.id });
-      if (actuator)
-        res
-          .status(200)
-          .json(new ApiResponse(res.statusCode.toString(), actuator));
+      const data=await database.getById("actuator",req);
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
       return;
     } catch (error) {
       next(error);
@@ -36,17 +29,9 @@ export default {
   },
   post: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const ActuatorPost = ActuatorPostSchema.parse(req.body);
-      const acutator = new models.Actuator(ActuatorPost);
-      await acutator.save();
-      res
-        .status(200)
-        .json(
-          new ApiResponse(res.statusCode.toString(), {
-            message: "created",
-            id: acutator.id,
-          })
-        );
+      const data=await database.post("actuator",req);
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      return;
     } catch (error) {
       next(error);
     }
@@ -54,27 +39,18 @@ export default {
   patch: async (req: Request, res: Response, next: NextFunction) => {
     try {
       
-      const ActuatorUpdate = ActuatorUpdateSchema.parse(req.body);
-      const actuator = await models.Actuator.findByIdAndUpdate(
-        req.params.id,
-        ActuatorUpdate,
-        { new: true }
-      );
-      res
-        .status(200)
-        .json(new ApiResponse(res.statusCode.toString(), actuator));
+      const data=await database.patch("actuator",req);
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      return;
     } catch (error) {
       next(error);
     }
   },
   delete: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const actuator = await models.Actuator.deleteOne({ _id: req.params.id });
-      res
-        .status(200)
-        .json(
-          new ApiResponse(res.statusCode.toString(), { message: "success" })
-        );
+      const data=await database.delete("actuator",req);
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), {message:data}));
+      return;
     } catch (error) {
       next(error);
     }
