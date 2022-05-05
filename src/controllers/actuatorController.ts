ee0a16370e679f370ee8823bfb466c13ad6ca6c5
NGUYEN Ngoc Dang <nguyen.ngocdang1996@gmail.com>
2022-05-05 17:23:19 +0200

    fix auth

--- src/controllers/actuatorController.ts
+++ src/controllers/actuatorController.ts
@@ -1,18 +1,13 @@
 import { Database } from "@/modules/Database";
 import { NextFunction, Request, Response } from "express";
-import models from "../models";
 import { ApiResponse } from "../modules/Response";
-import {
-  ActuatorPostSchema,
-  ActuatorUpdateSchema,
-} from "../types/actuatorSchema";
+
 let database=new Database;
-var xss = require("xss");
 export default {
   get: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data=await database.get("actuator",req);
-      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), {data}));
       return;
     } catch (error) {
       next(error);
@@ -21,7 +16,7 @@ export default {
   get_by_id: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data=await database.getById("actuator",req);
-      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), {data}));
       return;
     } catch (error) {
       next(error);
@@ -30,7 +25,7 @@ export default {
   post: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data=await database.post("actuator",req);
-      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), {data}));
       return;
     } catch (error) {
       next(error);
@@ -40,7 +35,7 @@ export default {
     try {
       
       const data=await database.patch("actuator",req);
-      res.status(200).json(new ApiResponse(res.statusCode.toString(), data));
+      res.status(200).json(new ApiResponse(res.statusCode.toString(), {data}));
       return;
     } catch (error) {
       next(error);
